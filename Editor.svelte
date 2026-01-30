26e0428b3c83c5a53b74d4cd7bd51389950edd0d
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2026-01-30 12:11:07 +0000

    PATH notebooks

--- Editor.svelte
+++ Editor.svelte
@@ -55,10 +55,13 @@
       if (lines[i].trim() === "") lineCount--;
       else break;
     }
+    console.log(diagnostics);
     return diagnostics.filter((diagnostic) => {
       if (!diagnostic.code || !diagnostic.code.value) return true;
-      if (diagnostic.code.value !== "reportUnusedExpression") return true;
-      return diagnostic.range.start.line + 1 !== lineCount;
+      if (diagnostic.code.value === "reportUnusedExpression")
+        return diagnostic.range.start.line + 1 !== lineCount;
+      if (diagnostic.code.value === "reportUndefinedVariable") return false;
+      return true;
     });
   };
 
@@ -280,6 +283,8 @@
     });
     return () => dispose?.();
   });
+
+  export const getModel = () => file;
 </script>
 
 <div style:width="100%" style:height="100%" bind:this={container}></div>
